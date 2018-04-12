8c15f9e902c53c51b23550f852c5cb5829844075
王松 <1109390306@qq.com>
2018-04-12 17:05:22 +0800

    Orleans 需要版本号为 1.5.3 OrleansConfiguration.xml 属性配置为 “较新则复制”

---
+++ OrleansTest/Grains/HelloGrain.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GrainInterfaces;
+
+namespace Grains
+{
+    class HelloGrain : Orleans.Grain, IHello
+    {
+        public Task<string> SayHello(string msg)
+        {
+            return Task.FromResult(string.Format("You said {0}, I say: Hello!", msg));
+        }
+    }
+}
